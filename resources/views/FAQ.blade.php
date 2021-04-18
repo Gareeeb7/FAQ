6067c39db705da904bd0a3fd945fd13427203938
Kunal <kunal9988243618@gmail.com>
2021-04-18 13:13:22 +0530

    Initial Commit

---
+++ resources/views/FAQ.blade.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>FAQ</title>
+</head>
+<style>
+    .faqHeader {
+        font-size: 27px;
+        margin: 20px;
+    }
+
+    .panel-heading [data-toggle="collapse"]:after {
+        font-family: 'Glyphicons Halflings';
+        content: "❯"; /* "play" icon */
+        float: right;
+        color: #F58723;
+        font-size: 18px;
+        line-height: 22px;
+        /* rotate "play" icon from > (right arrow) to down arrow */
+        -webkit-transform: rotate(-90deg);
+        -moz-transform: rotate(-90deg);
+        -ms-transform: rotate(-90deg);
+        -o-transform: rotate(-90deg);
+        transform: rotate(-90deg);
+    }
+
+    .panel-heading [data-toggle="collapse"].collapsed:after {
+        /* rotate "play" icon from > (right arrow) to ^ (up arrow) */
+        -webkit-transform: rotate(90deg);
+        -moz-transform: rotate(90deg);
+        -ms-transform: rotate(90deg);
+        -o-transform: rotate(90deg);
+        transform: rotate(90deg);
+        color: #454444;
+    }
+a:link, a:visited{ color: white;
+
+    }
+
+li{
+    display: inline;
+}
+</style >
+<body style="font-family:'Times New Roman', Times, serif; background-color: rgb(0, 0, 0)">
+<div style="background-color: orange" >  
+    <br >  
+    <center><h1>Frequently Asked Questions</h1></center>
+    <div class="container">
+        <br >
+        <br >
+        
+    
+        <div class="panel-group" id="accordion">
+            <div class="faqHeader">❯❯ General questions</div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Is account registration required?</a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                        Account registration at <strong>PrepBootstrap</strong> is only required if you will be selling or buying themes. 
+                        This ensures a valid communication channel for all parties involved in any transactions. 
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Can I submit my own Bootstrap templates or themes?</a>
+                    </h4>
+                </div>
+                <div id="collapseTen" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        A lot of the content of the site has been submitted by the community. Whether it is a commercial element/template/theme 
+                        or a free one, you are encouraged to contribute. All credits are published along with the resources. 
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">What is the currency used for all transactions?</a>
+                    </h4>
+                </div>
+                <div id="collapseEleven" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        All prices for themes, templates and other items, including each seller's or buyer's account balance are in <strong>USD</strong>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="faqHeader">❯❯ Sellers</div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Who cen sell items?</a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Any registed user, who presents a work, which is genuine and appealing, can post it on <strong>PrepBootstrap</strong>.
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">I want to sell my items - what are the steps?</a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        The steps involved in this process are really simple. All you need to do is:
+                        <ul>
+                            <li>Register an account</li>
+                            <li>Activate your account</li>
+                            <li>Go to the <strong>Themes</strong> section and upload your theme</li>
+                            <li>The next step is the approval step, which usually takes about 72 hours.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">How much do I get from each sale?</a>
+                    </h4>
+                </div>
+                <div id="collapseFive" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Here, at <strong>PrepBootstrap</strong>, we offer a great, 70% rate for each seller, regardless of any restrictions, such as volume, date of entry, etc.
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">Why sell my items here?</a>
+                    </h4>
+                </div>
+                <div id="collapseSix" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        There are a number of reasons why you should join us:
+                        <ul>
+                            <li>A great 70% flat rate for your items.</li>
+                            <li>Fast response/approval times. Many sites take weeks to process a theme or template. And if it gets rejected, there is another iteration. We have aliminated this, and made the process very fast. It only takes up to 72 hours for a template/theme to get reviewed.</li>
+                            <li>We are not an exclusive marketplace. This means that you can sell your items on <strong>PrepBootstrap</strong>, as well as on any other marketplate, and thus increase your earning potential.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight">What are the payment options?</a>
+                    </h4>
+                </div>
+                <div id="collapseEight" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        The best way to transfer funds is via Paypal. This secure platform ensures timely payments and a secure environment. 
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseNine">When do I get paid?</a>
+                    </h4>
+                </div>
+                <div id="collapseNine" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Our standard payment plan provides for monthly payments. At the end of each month, all accumulated funds are transfered to your account. 
+                        The minimum amount of your balance should be at least 70 USD. 
+                    </div>
+                </div>
+            </div>
+    
+            <div class="faqHeader">❯❯ Buyers</div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">I want to buy a theme - what are the steps?</a>
+                    </h4>
+                </div>
+                <div id="collapseFour" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Buying a theme on <strong>PrepBootstrap</strong> is really simple. Each theme has a live preview. 
+                        Once you have selected a theme or template, which is to your liking, you can quickly and securely pay via Paypal.
+                        <br />
+                        Once the transaction is complete, you gain full access to the purchased product. 
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">Is this the latest version of an item</a>
+                    </h4>
+                </div>
+                <div id="collapseSeven" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        Each item in <strong>PrepBootstrap</strong> is maintained to its latest version. This ensures its smooth operation.
+                    </div>
+                </div>
+                
+            </div>
+            <br><br><br><br>
+        </div>
+    </div>
+</div>
+    
+       <!-- Footer Links -->
+<div style="background-color: rgb(0, 0, 0); color: rgb(146, 143, 143);">
+<footer>
+   
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row" style="margin-top: 30px">
+        <br><br><br>
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3" >
+
+        <!-- Content --><br><br>
+        <h5 class="text-uppercase">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home TUTOR</h5>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Learn Anywhere Anytime.</p>
+
+      </div>
+      
+      <!-- Grid column -->
+
+     
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Social Media</h5>
+
+        <ul class="list-unstyled">
+          <li><a href="#">
+            <img src="images/twitter.png" width="32" height="30">
+          </a>
+          </li>
+          <li><a href="#">
+            <img src="images/insta.png" width="30" height="">
+          </a>
+          </li>
+          <li>
+            <a href="#">
+            <img src="images/facebook.png" width="35" height="32">
+            </a>
+          </li>
+          <li>
+            <a href="#">
+            <img src="images/linkedin.png" width="31" height="28">
+            </a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Address and contact</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            Civil City
+          </li>
+          <li>
+            141001, Ludhiana
+          </li>
+          <li>
+            E-MAil: Kunal9988243618@gmail.com
+          </li>
+          <li>
+            Phone: 9877769078
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div  class="footer-copyright text-center py-3">
+      
+    <p>© 2021 Copyright: HOME TUTOR.com</p>
+    <p> ~ Designed by Kunal Malhotra</p>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+</div>
+<!-- Footer -->
+</body>
+</html>
